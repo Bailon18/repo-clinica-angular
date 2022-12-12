31f0725a7272d2900ddc757fd86d6923fe2a40ee
Bailon18 <paucarmontesbailon@gmail.com>
2022-12-12 12:18:18 -0500

    se actualizo los tipo de datos    -BAILON

--- src/app/componentes/dashboard/citas/citas.component.ts
+++ src/app/componentes/dashboard/citas/citas.component.ts
@@ -1,5 +1,5 @@
 import { formatDate } from '@angular/common';
-import { Component, OnInit , ViewEncapsulation} from '@angular/core';
+import { Component, OnChanges, OnInit , SimpleChanges, ViewEncapsulation} from '@angular/core';
 import { MatDatepickerInputEvent } from '@angular/material/datepicker';
 import { Paciente } from '../paciente/models/paciente';
 import { Citas, CitasResultado } from './model/citas';
@@ -8,6 +8,7 @@ import { Usuario } from '../usuario/model/usuario';
 
 
 import {MatCalendarCellClassFunction} from '@angular/material/datepicker';
+import { map } from 'rxjs';
 
 
 @Component({
@@ -18,22 +19,21 @@ import {MatCalendarCellClassFunction} from '@angular/material/datepicker';
 })
 export class CitasComponent implements OnInit {
 
+    
     id!:number;
     nombrepsicologa!:string;
-    fecha: string;
+    fecha!: Date;
     psicologos : Usuario[];
     fechatext:string;
     selected: Date | null;
-
-    dateClass:MatCalendarCellClassFunction<Date> ;
-
-
-
-
-
     public citas = new Citas();
-
+    public resul:any[];
+    dias:number[];
+    dato:number;
     
+    dateClass:MatCalendarCellClassFunction<Date> ;
+    mesactual:number;
+
 
     public horadefinida:CitasResultado[]= [
         {hora:1, formato:"1:00 AM", dato:this.citas}, {hora:1, formato:"2:00 AM", dato:this.citas}, {hora:1, formato:"3:00 AM", dato:this.citas},
@@ -47,73 +47,183 @@ export class CitasComponent implements OnInit {
 
     public listacitasresul:any[];
 
-    constructor(private servicio: CitasService) { }
+
+    funcion():MatCalendarCellClassFunction<Date>{
+
+
+        //this.dato = JSON.parse(localStorage.getItem('mes')!) || [];
+     
+        //localStorage.setItem('mes', JSON.stringify(this.dato))
+
+        //let dato2 = JSON.parse(localStorage.getItem('mes')!) || [];
+
+        //console.log("MES ACTUAL 2 ", dato2);
+        //console.log("funcion")
+
+        this.dateClass = (cellDate, view) => {
+
+            console.log("Dateclass",);
+            
+            if (view === 'month') {
+
+                //let mesesito = JSON.parse(localStorage.getItem('mes')!) || [];
+                let dias = JSON.parse(localStorage.getItem('dias')!) || []
+
+                
+                
+                // if(mes != undefined){
+                    
+                //     //console.log("cellDate.getMonth()+1 ", cellDate.getMonth()+1)
+                //     //console.log("MES ", mesesito)
+                //     //console.log("dias ", dias)
+                    
+
+                //     if(cellDate.getMonth()+1 === 12 ){
+
+                //         //console.log("INGRESO")
+                    
+                //         let date = cellDate.getDate();
+                        
+                //         let dias = JSON.parse(localStorage.getItem('dias')!) || []
+
+                    
+                //         for(let i of dias){
+        
+                //             if(date == i){
+                //                 return 'day-active';
+                //             }
+                //         } 
+                //     }
+                // }
+            
+            }
+            return '';
+
+        }
+
+        
+
+        return this.dateClass;
+        
+    }
+
+
+
+    constructor(private servicio: CitasService) {
+     }
+
+
 
     ngOnInit(): void {
 
-        this.validarfechascita();
- 
-        // solo guardamos la lista pero con psicologos
+        localStorage.setItem('mes', JSON.stringify(12));
+
+        this.funcion();
+
         this.servicio.listarPsicologas().subscribe(resultado => {
             this.psicologos = resultado.filter(psicologo => {
                 let res = psicologo.roles.some( r => r.id === 3 )
                 return res;
             })
-            console.log("USUARIOS ", this.psicologos);
+            //console.log("USUARIOS ", this.psicologos);
         })
 
         this.listacitasresul = this.horadefinida.splice(7,20);
-        console.log("LISTAS ", this.listacitasresul)
+        //console.log("LISTAS ", this.listacitasresul)
         this.fechatext = new Date().toLocaleDateString('es-PE', { weekday:"long", year:"numeric", month:"short", day:"numeric"}) 
+        
     }
+
 
     validarfechascita(){
 
-        // consulta 
-        // (fecha, psicologa)
-        // return -> lista de dias de esa fecha que tiene cita y luego pintamos
-        
-        this.dateClass = (cellDate, view) => {
-        
-            if (view === 'month') {
-    
-              const date = cellDate.getDate();
-              
-              return date === 10 || date === 20 ? 'day-active' : '';
-            }
-        
-            return '';
-        };
+        let mes = JSON.parse(localStorage.getItem('mes')!) || [];
+
+        //console.log("MES ACTUAL EN validar ", mes);
+
+        this.servicio.listardias(mes).subscribe(res => {
+            localStorage.setItem('dias', JSON.stringify(res))
+        });
+
+        //this.dias = JSON.parse(localStorage.getItem('dias')!) || [];
+        //console.log("DIAS en validar ", this.dias);
+
+        this.funcion();
     }
 
+    holaaa(event:any){
 
+        let calendario = document.getElementById("calendario")?.innerText.slice(0,3).toString().trim();
+
+        let mapa= new Map();
+
+        mapa.set("JAN",1);
+        mapa.set("FEB",2)
+        mapa.set("MAR",3)
+        mapa.set("APR",4)
+        mapa.set("MAY",5)
+        mapa.set("JUN",6)
+        mapa.set("JUL",7)
+        mapa.set("AUG",8)
+        mapa.set("SEP",9)
+        mapa.set("OCT",10)
+        mapa.set("NOV",11)
+        mapa.set("DEC",12)
+        
+        let mes;
+        
+        for (let clave of mapa.keys()) {
+            
+            if(clave === calendario)
+                mes = mapa.get(calendario);
+                
+            
+        }
+
+        //console.log("Ddddddddd ", mes);
+        localStorage.setItem('mes', JSON.stringify(mes))
+        this.validarfechascita();
+        
+     
+        
+    }
 
     obtenerfecha(event:any){
-        this.fecha = formatDate(event, 'yyyy-MM-dd', 'en-PE').toString();
+
+        this.fecha = event
         this.fechatext = event.toLocaleDateString('es-PE', { weekday:"long", year:"numeric", month:"short", day:"numeric"}) 
-        console.log("FECHA", this.fecha);
+        //console.log("FECHJA ", this.fechatext)
         this.valirdarenviodatos();
     }
 
     obtenerpsicologo(event:any){
+        console.log("EVENTO COMBOBOX")
         this.id = event.id;
         this.nombrepsicologa = event.nombres +" "+ event.apellidos
         console.log("ID ", this.id);
-        this.valirdarenviodatos();
+
+    
+        // if(this.fecha == undefined)
+        //     fechadefaul = new Date().getMonth()+1;
+        //     this.validarfechascita(fechadefaul);
+        
+
     }
 
     valirdarenviodatos(){
 
+        //this.validarfechascita(new Date().getMonth()+1)
+
         if(this.fecha != null && this.id != null){
             this.listarCitas(this.id, this.fecha);
+
+            
         }
 
     }
     
 
-    listarCitas(id:number , fecha: string){
-
-
+    listarCitas(id:number , fecha: Date){
         this.servicio.buscarCitas(id, fecha).subscribe(res =>{
 
             this.listacitasresul = [
--- src/app/componentes/dashboard/citas/services/citas.service.ts
+++ src/app/componentes/dashboard/citas/services/citas.service.ts
@@ -12,17 +12,22 @@ export class CitasService {
 
   private urlbuscarcitas: string = 'http://localhost:8080/citas/buscarcitas';
   private urlListar: string = 'http://localhost:8080/usuario/listar';
+  private urldias : string ='http://localhost:8080/citas/listardias';
 
   private httpHeaders = new HttpHeaders({'Content-Type':'application/json'});
 
   constructor(private http: HttpClient) { }
 
-  buscarCitas(id:number, fecha:string): Observable<Citas[]>{
+  buscarCitas(id:number, fecha:Date): Observable<Citas[]>{
     return this.http.get<Citas[]>(`${this.urlbuscarcitas}/${id}/${fecha}`);
   }
 
   listarPsicologas(): Observable<Usuario[]>{
       return this.http.get<Usuario[]>(this.urlListar);
+  }
+
+  listardias(mes: number): Observable<number[]>{
+    return this.http.get<number[]>(`${this.urldias}/${mes}`);
   }
 }
 
