f8943c3799c89665c6a6c34bb23ca90b683565ff
Bailon18 <paucarmontesbailon@gmail.com>
2022-12-13 06:19:25 -0500

    se quito la implementacion de dias agendados    -BAILON

--- src/app/componentes/dashboard/citas/citas.component.ts
+++ src/app/componentes/dashboard/citas/citas.component.ts
@@ -1,15 +1,10 @@
-import { formatDate } from '@angular/common';
-import { AfterViewInit, Component, OnChanges, OnInit , Renderer2, ViewChild, ViewEncapsulation} from '@angular/core';
-import { MatCalendar, MatCalendarCellCssClasses, MatDatepickerInputEvent } from '@angular/material/datepicker';
-import { Paciente } from '../paciente/models/paciente';
+
+import { Component, OnInit , ViewEncapsulation} from '@angular/core';
 import { Citas, CitasResultado } from './model/citas';
 import { CitasService } from './services/citas.service';
 import { Usuario } from '../usuario/model/usuario';
-
-
 import {MatCalendarCellClassFunction} from '@angular/material/datepicker';
-import { map } from 'rxjs';
-import { NativeDateAdapter } from '@angular/material/core';
+
 
 
 @Component({
@@ -18,9 +13,8 @@ import { NativeDateAdapter } from '@angular/material/core';
   styleUrls:['./citas.component.css'],
   encapsulation: ViewEncapsulation.None,
 })
-export class CitasComponent implements OnInit, AfterViewInit {
+export class CitasComponent implements OnInit {
 
-    
     id!:number;
     nombrepsicologa!:string;
     fecha!: Date;
@@ -32,11 +26,6 @@ export class CitasComponent implements OnInit, AfterViewInit {
     dias:number[];
     dato:number;
     
-    dateClass:MatCalendarCellClassFunction<Date> ;
-    mesactual:number;
-
-
-
     public horadefinida:CitasResultado[]= [
         {hora:1, formato:"1:00 AM", dato:this.citas}, {hora:1, formato:"2:00 AM", dato:this.citas}, {hora:1, formato:"3:00 AM", dato:this.citas},
         {hora:1, formato:"4:00 AM", dato:this.citas},{hora:1, formato:"5:00 AM", dato:this.citas}, {hora:1, formato:"6:00 AM", dato:this.citas},
@@ -50,177 +39,20 @@ export class CitasComponent implements OnInit, AfterViewInit {
     public listacitasresul:any[];
 
 
-    funcion():MatCalendarCellClassFunction<Date>{
-
-        //this.dato = JSON.parse(localStorage.getItem('mes')!) || [];
-     
-        //localStorage.setItem('mes', JSON.stringify(this.dato))
-
-        //let dato2 = JSON.parse(localStorage.getItem('mes')!) || [];
-
-        //console.log("MES ACTUAL 2 ", dato2);
-        //console.log("funcion")
-
-        this.dateClass = (cellDate, view) => {
-
-            console.log("Dateclass",);
-            
-            if (view === 'month') {
-
-                //let mesesito = JSON.parse(localStorage.getItem('mes')!) || [];
-                let dias = JSON.parse(localStorage.getItem('dias')!) || []
-
-                // if(mes != undefined){
-                    
-                //     //console.log("cellDate.getMonth()+1 ", cellDate.getMonth()+1)
-                //     //console.log("MES ", mesesito)
-                //     //console.log("dias ", dias)
-                    
-
-                //     if(cellDate.getMonth()+1 === 12 ){
-
-                //         //console.log("INGRESO")
-                    
-                //         let date = cellDate.getDate();
-                        
-                //         let dias = JSON.parse(localStorage.getItem('dias')!) || []
-
-                    
-                //         for(let i of dias){
-        
-                //             if(date == i){
-                //                 return 'day-active';
-                //             }
-                //         } 
-                //     }
-                // }
-            
-            }
-            return '';
-
-        }
-
-        
-
-        return this.dateClass;
-        
-    }
-
-
-
-   
-    constructor(private servicio: CitasService, private renderer: Renderer2 ) {
-     }
-
-     ngAfterViewInit(): void {
-
-        const BACK_BUTTON = document.querySelector('.mat-calendar-previous-button');
-        const FORWARD_BUTTON = document.querySelector('.mat-calendar-next-button');
-   
-        if (BACK_BUTTON) {
-          this.renderer.listen(BACK_BUTTON, 'click', (event) => {
-            
-          });
-        }
-        if (FORWARD_BUTTON) {
-          this.renderer.listen(FORWARD_BUTTON, 'click', (event) => {
-
-          });
-        }
-      }
-
-    handleMonthSelected(event:any) {
-        console.log("Month changed", event);
-        //handler logic
-    }
-
-
-
-    hola(event:any){
-     
-        let elements = document.querySelectorAll('.calendario');
-
-        console.log("caaa ", elements)
-        
-        let x =  elements[0].querySelectorAll('.mat-calendar-body-header-content');
-        x.forEach(y => {
-            //y.classList.add('newClass');
-            console.log("YYY ",y)
-        });
-    
+    constructor(private servicio: CitasService ) {
     }
 
     ngOnInit(): void {
-
-        localStorage.setItem('mes', JSON.stringify(12));
-
-        this.funcion();
 
         this.servicio.listarPsicologas().subscribe(resultado => {
             this.psicologos = resultado.filter(psicologo => {
                 let res = psicologo.roles.some( r => r.id === 3 )
                 return res;
             })
-            //console.log("USUARIOS ", this.psicologos);
         })
 
         this.listacitasresul = this.horadefinida.splice(7,20);
-        //console.log("LISTAS ", this.listacitasresul)
         this.fechatext = new Date().toLocaleDateString('es-PE', { weekday:"long", year:"numeric", month:"short", day:"numeric"}) 
-        
-    }
-
-
-    validarfechascita(){
-
-        let mes = JSON.parse(localStorage.getItem('mes')!) || [];
-
-        //console.log("MES ACTUAL EN validar ", mes);
-
-        this.servicio.listardias(mes).subscribe(res => {
-            localStorage.setItem('dias', JSON.stringify(res))
-        });
-
-        //this.dias = JSON.parse(localStorage.getItem('dias')!) || [];
-        //console.log("DIAS en validar ", this.dias);
-
-        this.funcion();
-    }
-
-    holaaa(event:any){
-
-        let calendario = document.getElementById("calendario")?.innerText.slice(0,3).toString().trim();
-
-        let mapa= new Map();
-
-        mapa.set("JAN",1);
-        mapa.set("FEB",2)
-        mapa.set("MAR",3)
-        mapa.set("APR",4)
-        mapa.set("MAY",5)
-        mapa.set("JUN",6)
-        mapa.set("JUL",7)
-        mapa.set("AUG",8)
-        mapa.set("SEP",9)
-        mapa.set("OCT",10)
-        mapa.set("NOV",11)
-        mapa.set("DEC",12)
-        
-        let mes;
-        
-        for (let clave of mapa.keys()) {
-            
-            if(clave === calendario)
-                mes = mapa.get(calendario);
-                
-            
-        }
-
-        //console.log("Ddddddddd ", mes);
-        localStorage.setItem('mes', JSON.stringify(mes))
-        this.validarfechascita();
-        
-     
         
     }
 
@@ -228,34 +60,21 @@ export class CitasComponent implements OnInit, AfterViewInit {
 
         this.fecha = event
         this.fechatext = event.toLocaleDateString('es-PE', { weekday:"long", year:"numeric", month:"short", day:"numeric"}) 
-        //console.log("FECHJA ", this.fechatext)
         this.valirdarenviodatos();
     }
 
     obtenerpsicologo(event:any){
-        console.log("EVENTO COMBOBOX")
         this.id = event.id;
         this.nombrepsicologa = event.nombres +" "+ event.apellidos
         console.log("ID ", this.id);
-
-    
-        // if(this.fecha == undefined)
-        //     fechadefaul = new Date().getMonth()+1;
-        //     this.validarfechascita(fechadefaul);
-        
-
     }
 
     valirdarenviodatos(){
 
-        //this.validarfechascita(new Date().getMonth()+1)
-
         if(this.fecha != null && this.id != null){
             this.listarCitas(this.id, this.fecha);
 
-            
         }
-
     }
     
 
@@ -281,9 +100,6 @@ export class CitasComponent implements OnInit, AfterViewInit {
 
                 this.listacitasresul = this.listacitasresul.splice(7,20);
             }
-
-            console.log("LISTA FINAL ", this.listacitasresul);
-
         }
         )
     }
